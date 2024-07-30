2ba1bad8e402b0d16e43ee88817106fca742585d
JaraPava <alberto-11111@live.com.mx>
2024-07-30 14:11:27 -0600

    Se añadieron más rutas para la parte de redireccionar cualquier pagina que este mal escrita en la url al Home de la aplicación en el archivo app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.component'),
-    children: []
-  },
-  {
-   path:'',
-   redirectTo:'/home',
-   pathMatch:'full'
-  }
+export const routes: Routes =
+  [
+    {
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+    },
+    {
+      path: 'home',
+      loadComponent: () => import('./home/home.component'),
+      children: []
+    },
+    {
+      path: '**',
+      redirectTo: '/home',
+      pathMatch: 'full'
+    }
 
-];
+  ];
